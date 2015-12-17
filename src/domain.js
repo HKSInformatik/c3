89d7b157215de643dcf4cda4256ca00767d958c6
Gökhan Özen <Goekhan.Oezen@hksinformatik.de>
2015-12-17 09:02:13 +0100

    clone y2-axis domain from y-axis if no data is bound to y2

--- src/domain.js
+++ src/domain.js
@@ -81,7 +81,7 @@ c3_chart_internal_fn.getYDomain = function (targets, axisId, xDomain) {
     yDomainMax = isValue(yMax) ? yMax : isValue(yMin) ? (yMin < yDomainMax ? yDomainMax : yMin + 10) : yDomainMax;
 
     if (yTargets.length === 0) { // use current domain if target of axisId is none
-        return axisId === 'y2' ? $$.y2.domain() : $$.y.domain();
+        return axisId === 'y2' ? $$.y.domain() : $$.getYDomain(targets, 'y2', false);
     }
     if (isNaN(yDomainMin)) { // set minimum to zero when not number
         yDomainMin = 0;
